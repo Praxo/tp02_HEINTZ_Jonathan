3ab437f9fba847ed2239a08ed9ac42cba156dda6
Praxo <horus.shark73@gmail.com>
2021-12-09 00:48:51 +0100

    ajout des conditions

--- FRONTEND/src/app/register/register.component.ts
+++ FRONTEND/src/app/register/register.component.ts
@@ -26,6 +26,48 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
+    if (this.firstName === "") {
+      alert("Le champ nom ne doit pas être vide");
+      return;
+    }
+    if (this.lastName === "") {
+      alert("Le champ prénom ne doit pas être vide");
+      return;
+    }
+    if (this.email === "" || !this.email.includes("@") || !this.email.includes(".")) {
+      alert("Le champ email ne doit pas être vide");
+      return;
+    }
+    if (this.login === "") {
+      alert("Le champ login ne doit pas être vide");
+      return;
+    }
+    if (this.password !== this.passwordConfirmation) {
+      alert("Les mots de passe sont différents");
+      return
+    }
+    if (this.password === "") {
+      alert("Le mot de passe ne doit pas être vide");
+      return;
+    }
+    if (this.passwordConfirmation === "") {
+      alert("Le mot de passe de confirmation ne doit pas être vide");
+      return;
+    }
+    if (this.zipCode === 0) {
+      alert("Le code postale ne peut pas être à 0");
+      return;
+    }
+    if (this.adress === "") {
+      alert("Le champ adresse ne doit pas être vide");
+      return;
+    }
+    if (this.phoneNumber === "") {
+      alert("Le champ téléphone ne doit pas être vide");
+      return;
+    }
+
+
     this.showData = true;
   }
 
