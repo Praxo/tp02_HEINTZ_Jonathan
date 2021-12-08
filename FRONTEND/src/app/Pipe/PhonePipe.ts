e4a0aae6485d8280a6c5e1b7dea4135e1f44fe70
Praxo <horus.shark73@gmail.com>
2021-12-08 23:49:38 +0100

    correction pipe

--- FRONTEND/src/app/Pipe/PhonePipe.ts
+++ FRONTEND/src/app/Pipe/PhonePipe.ts
@@ -1,19 +1,10 @@
-import { Component, Pipe } from "@angular/core";
+import { Component, Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
   name: "phone"
 })
 export class PhonePipe {
-  transform(rawNum: any) {
-    rawNum = rawNum.charAt(0) != 0 ? "0" + rawNum : "" + rawNum;
-
-    let newStr = "";
-    let i = 0;
-
-    for (; i < Math.floor(rawNum.length / 2) - 1; i++) {
-      newStr = newStr + rawNum.substr(i * 2, 2) + "-";
-    }
-
-    return newStr + rawNum.substr(i * 2);
+  transform(value: any, ...args: any): any {
+    return "(+33)" + value.substr(1);
   }
 }
